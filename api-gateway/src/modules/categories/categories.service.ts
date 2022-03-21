31cca17d641fd3a765c5a123f6e81b31bd9427ad
venzel <eneas.eng@yahoo.com>
2022-03-20 23:48:14 -0300

    fix: modifica modulos

--- api-gateway/src/modules/categories/categories.service.ts
+++ api-gateway/src/modules/categories/categories.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
 import { CreateCategoryDto } from './dtos/create.category.dto';
 import { UpdateCategoryDto } from './dtos/update.category.dto';
 import { ClientProxyProduct } from '../../proxyrmq/client.proxy';
 
 @Injectable()
 export class CategoriesService {
-    private clientAdminBackend = this.clientProxyProduct.getClientProxyAdminBackendInstance();
+    private clientAdminBackend =
+        this.clientProxyProduct.getClientProxyAdminBackendInstance();
 
     constructor(private clientProxyProduct: ClientProxyProduct) {}
 
@@ -15,8 +17,8 @@ export class CategoriesService {
         this.clientAdminBackend.emit('create-category', createCategoryDto);
     }
 
-    findById(id: string): Promise<any> {
-        return this.clientAdminBackend.send('consultar-categorias', id ? id : '').toPromise();
+    findById(id: string): Observable<any> {
+        return this.clientAdminBackend.send('find-category-by-id', id ? id : '');
     }
 
     update(updateCategoryDto: UpdateCategoryDto, id: string) {
--- categories/src/modules/categories/categories.service.ts
+++ categories/src/modules/categories/categories.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoryRepository } from './category.repository';
 import { CreateCategoryDto } from './dtos/create.category.dto';
 
 @Injectable()
 export class CategoriesService {
-    constructor(@InjectRepository(CategoryRepository) private categoryRepository: CategoryRepository) {}
+    logger = new Logger(CategoriesService.name);
+
+    constructor(
+        @InjectRepository(CategoryRepository)
+        private categoryRepository: CategoryRepository
+    ) {}
 
     async create(createCategoryDto: CreateCategoryDto): Promise<void> {
-        console.log('CRIADA E INSERIDA NO BANCO DE DADOS!');
+        await this.categoryRepository.createAndSave(createCategoryDto);
+
+        this.logger.log(`data: ${JSON.stringify(createCategoryDto)}`);
     }
 }
